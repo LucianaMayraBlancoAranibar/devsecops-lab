aeb4a258d14b03ea4913437507a11362a765eb61
LucianaMayraBlancoAranibar <blancoaranibarlu@gmail.com>
2026-08-01 11:23:23 -0400

    fix: ajustar reglas estrictas de sast e iac

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,46 @@
+#checkov:skip=CKV_AWS_18: "Bucket logging no requerido en lab"
+#checkov:skip=CKV_AWS_144: "Replicacion no requerida en lab"
+
 provider "aws" {
   region = "us-east-1"
 }
 
-# FALLA IaC 1: S3 Bucket público sin cifrado
-resource "aws_s3_bucket" "bucket_inseguro" {
+resource "aws_s3_bucket" "bucket_seguro" {
   bucket = "mi-bucket-devsecops-demo-12345"
 }
 
 resource "aws_s3_bucket_public_access_block" "publico" {
-  bucket = aws_s3_bucket.bucket_inseguro.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  bucket                  = aws_s3_bucket.bucket_seguro.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-# FALLA IaC 2: Puerto SSH expuesto a todo Internet
-resource "aws_security_group" "sg_inseguro" {
-  name = "sg_ssh_abierto"
+resource "aws_s3_bucket_server_side_encryption_configuration" "cifrado" {
+  bucket = aws_s3_bucket.bucket_seguro.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versionado" {
+  bucket = aws_s3_bucket.bucket_seguro.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_security_group" "sg_seguro" {
+  name        = "sg_ssh_restringido"
+  description = "Grupo de seguridad restringido para lab"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
